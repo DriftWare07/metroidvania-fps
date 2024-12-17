23773a1010dcec6290451879884d3b77e1db4161
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2024-12-17 15:43:02 -0600

    weapon door added
    this door opens when damaged, and then automatically closes after 5 seconds.

--- scenes/doors/doorhandler.gd
+++ scenes/doors/doorhandler.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 
 @export var timeout = 10
+@export var close_on_timeout = false
 @export var animation : AnimationPlayer
 @export var open_anim = ""
 @export var collider : CollisionShape3D
@@ -12,16 +13,34 @@ signal doorStateChanged
 signal doorOpened
 signal doorShut
 
+var t = 0
+
+func _ready() -> void:
+	t = timeout
+
+func _process(delta: float) -> void:
+	if not close_on_timeout: return
+	t -= delta
+	
+	if state and t < 0:
+		close()
+		
+		
+
 func open():
 	if state:return
 	state = true
 	animation.play(open_anim)
 	collider.disabled = true
+	t = timeout
 	doorStateChanged.emit()
 	doorOpened.emit()
 
 func close():
 	if not state: return
+	
+	
+	
 	state = false
 	animation.play_backwards(open_anim)
 	collider.disabled = false
